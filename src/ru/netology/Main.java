eb5353ac999e2b47c2ca98390e36db775c521700
LeraLem <liemiatskikh@mail.ru>
2023-02-05 16:39:04 +0500

    second commit

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -3,8 +3,8 @@ package ru.netology;
 public class Main {
 
     public static void main(String[] args) {
-        Worker.OnTaskDoneListener listener = System.out::println;
-        Worker.OnTaskErrorListener listener2 = System.out::println;
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listener2 = System.out::println;
         Worker worker = new Worker(listener, listener2);
         worker.start();
     }
--- src/ru/netology/Worker.java
+++ src/ru/netology/Worker.java
@@ -4,16 +4,6 @@ public class Worker {
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-    @FunctionalInterface
-    public interface OnTaskDoneListener {
-        void onDone(String result);
-    }
-
-    @FunctionalInterface
-    public interface OnTaskErrorListener {
-        void onError(String result);
-    }
-
     public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
         this.errorCallback = errorCallback;
